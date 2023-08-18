8771b6e63c7ebdbf340969c772a7d1cc3051fad6
Luca Di Gangi <57185672+lucadigangi@users.noreply.github.com>
2023-08-18 17:01:25 +0200

    Update main.py
    aggiunta modifica per considerare studenti che hanno espresso una sola preferenza e studenti che non hanno espresso preferenze

--- main.py
+++ main.py
@@ -15,20 +15,27 @@ def generate_student_dictionary(file_path):
     
     return students
 
-def assign_classes(students, teachers):
+def assign_classes(students, teachers, max_students_per_class):
     class_assignments = {teacher: {1: [], 2: []} for teacher in teachers}
     companions = {student['name']: student['companion_preference'] for student in students}
     
-    for student in students:
-        teacher_preferences = student['teacher_preferences']
-        
-        for teacher in teacher_preferences:
-            for class_num in [1, 2]:
-                if len(class_assignments[teacher][class_num]) < 3:
+    single_preference_students = [student for student in students if len(student['teacher_preferences']) == 1]
+    other_students = [student for student in students if len(student['teacher_preferences']) == 0]
+    
+    # Assign students with single preference
+    for student in single_preference_students:
+        teacher = student['teacher_preferences'][0]
+        class_num = 1 if len(class_assignments[teacher][1]) < max_students_per_class else 2
+        class_assignments[teacher][class_num].append(student['name'])
+        companions[student['companion_preference']].remove(student['name'])
+    
+    # Assign other students
+    for teacher in teachers:
+        for class_num in [1, 2]:
+            while len(class_assignments[teacher][class_num]) < max_students_per_class:
+                if other_students:
+                    student = other_students.pop(0)
                     class_assignments[teacher][class_num].append(student['name'])
-                    if student['companion_preference'] in companions and student['name'] in companions[student['companion_preference']]:
-                        companions[student['companion_preference']].remove(student['name'])
-                    break
     
     return class_assignments
 
@@ -36,8 +43,9 @@ def assign_classes(students, teachers):
 
 teachers = ['carla', 'marta']
 file_path = "/content/sample_data/Cartel1.csv"  # Assicurati che il percorso sia corretto
+max_students_per_class = 20  # Modifica questo valore se necessario
 
 # Run the algorithm
 students = generate_student_dictionary(file_path)
-class_assignments = assign_classes(students, teachers)
+class_assignments = assign_classes(students, teachers, max_students_per_class)
 print(class_assignments)
